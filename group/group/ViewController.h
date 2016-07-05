4279e5df5c13a3cf11b5136fcfe56137d0b7dbc1
lilei <lilei@lileismacbook-pro.local>
2016-07-05 18:39:10 +0800

    初始化项目

---
+++ group/group/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  group
+//
+//  Created by lilei on 16/7/5.
+//  Copyright © 2016年 lilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ group/groupTests/groupTests.m
@@ -0,0 +1,39 @@
+//
+//  groupTests.m
+//  groupTests
+//
+//  Created by lilei on 16/7/5.
+//  Copyright © 2016年 lilei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface groupTests : XCTestCase
+
+@end
+
+@implementation groupTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
