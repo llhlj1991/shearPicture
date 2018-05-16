936142ccfcbdb990aeda9a60292caef1814a4379
lilei <lilei@lilei.local>
2018-05-16 16:54:03 +0800

    Student

---
+++ group/group/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  shearPicture
+//
+//  Created by lilei on 2018/5/16.
+//  Copyright © 2018年 lilei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
